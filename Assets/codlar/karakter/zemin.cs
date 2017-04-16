be6019030f670062db7a9a51fb76df6afc971162
ayshe <140315050@ogr.cbu.edu.tr>
2017-04-17 01:44:21 +0300

    updatee

---
+++ Assets/codlar/karakter/zemin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zemin : MonoBehaviour {
+
+    Karakter kr;
+	void Start () {
+        kr = transform.root.gameObject.GetComponent<Karakter>();//ayaklarına koydugumuz box collidorı biz oluştururken karaktere sag tıklamıştık  yani burda root ona diyoruz ve ona yönlendiriyoruz.
+
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   void OnTriggerEnter2D()
+    {
+        kr.yerdemi = true;
+
+    }
+    
+   void OnTriggerStay2D()
+    {
+        kr.yerdemi = true;
+    }
+
+   void OnTriggerExit2D()
+    {
+        kr.yerdemi = false;
+
+    }
+}
